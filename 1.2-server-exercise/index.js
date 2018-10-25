292cc54065e87d029eaebbb0c1e9c102e0de85d3
yona <04l.yona@gmail.com>
2018-10-25 11:07:50 -0400

    Define typeDefs and resolvers and set up server

--- 1.2-server-exercise/index.js
+++ 1.2-server-exercise/index.js
@@ -12,3 +12,32 @@ const frameworks = [
     stars: 104483
   }
 ];
+
+const typeDefs = gql`
+  type framework {
+    title: String
+    git: String
+    stars: Int
+  }
+
+  type Query {
+    allFrameworks: [framework]
+    Framework(title: String): framework
+  }
+  `
+
+const resolvers = {
+  Query: {
+    allFrameworks: () => frameworks,
+    Framework: (_, {title}) => {
+      return frameworks.find(framework => framework.title == title)
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(data => console.log(`server started at port ${data.port}`))
